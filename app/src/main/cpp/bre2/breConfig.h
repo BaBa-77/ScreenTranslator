02ead925d15ad9866a3c43bf0cf4d7f0d7739b35
usernameak <sitemakers.3dn.ru@gmail.com>
2021-09-17 16:16:07 +0200

    Disable platform debug

--- app/src/main/cpp/bre2/breConfig.h
+++ app/src/main/cpp/bre2/breConfig.h
@@ -5,7 +5,7 @@
 #define BRE_CONFIG_ESN 0xab2b3c4f
 #define BRE_CONFIG_IMEI "1234567812345678"
 #define BRE_CONFIG_HEAP_SIZE (32 * 1024 * 1024)
-#define BRE_ENABLE_PLATFORM_DEBUG
+// #define BRE_ENABLE_PLATFORM_DEBUG
 
 // GPS timestamp; set to zero if you want to use system time
 #define BRE_FORCED_TIME 996877357ULL
