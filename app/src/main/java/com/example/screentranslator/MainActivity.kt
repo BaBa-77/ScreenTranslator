1ad91dda213b7e493938aa547f1440f3b8ffcfea
BaBa-77 <55563860+BaBa-77@users.noreply.github.com>
2025-07-10 11:27:15 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/screentranslator/MainActivity.kt
+++ app/src/main/java/com/example/screentranslator/MainActivity.kt
@@ -5,6 +5,11 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
